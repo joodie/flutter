8a9380e33b58e6479d4961ddf390276b1662f443
Joost Diepenmaat <joost@zeekat.nl>
2011-03-26 14:16:21 +0100

    prepare 0.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flutter "0.0.1-SNAPSHOT"
+(defproject flutter "0.0.1"
   :description "consistent, extensible, hiccup-based HTML form fields"
   :dependencies [[hiccup "0.3.4"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
