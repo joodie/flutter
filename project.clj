46e5761cfd20e401f2a98115916c445cfbced16f
Joost Diepenmaat <joost@zeekat.nl>
2011-03-26 13:14:50 +0100

    fix project name and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject anansi-forms "0.0.1"
-  :description "consistent model of hiccup forms"
+(defproject flutter "0.0.1-SNAPSHOT"
+  :description "consistent, extensible, hiccup-based HTML form fields"
   :dependencies [[hiccup "0.3.4"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
