959165820b543a5a4b79a0a8f904428cb1d854c1
Joost Diepenmaat <joost@zeekat.nl>
2011-03-28 22:26:29 +0200

    and this is why 'name' is not a good idea for a core function.

--- src/flutter/params.clj
+++ src/flutter/params.clj
@@ -7,13 +7,13 @@ like wrap-optionals, attrs and opts are optional, but attrs must be a map.
 when given the full five parameters, override the values in the params map."
   [f params]
   (fn
-    ([type attrs name opts]
-       (f type attrs name opts (params name)))
     ([type attrs name opts value]
        (f type attrs name opts value))
-    ([type name]
-       (f type nil name nil (params name)))
+    ([type attrs name opts]
+       (f type attrs name opts (params name)))
     ([type a2 a3]
-       (if (map? a2)
-         (f type a2 a3 nil (params name))
-         (f type nil a2 a3 (params name))))))
+       (if (map? a2) 
+         (f type a2 a3 nil (params a3))
+         (f type nil a2 a3 (params a2))))
+    ([type name]
+       (f type nil name nil (params name)))))
