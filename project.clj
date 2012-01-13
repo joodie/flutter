336e40386ff4e79ce9243ec8690e2a62c41f80a3
Joost Diepenmaat <joost@zeekat.nl>
2012-01-13 08:57:21 +0100

    update to clojure-1.3 friendly version of hiccup

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject flutter "0.0.7"
   :description "consistent, extensible, hiccup-based HTML form fields"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "0.3.4"]])
+                 [hiccup "0.3.8"]])
 
