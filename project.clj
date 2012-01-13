a9e02eea18a6c73c5d97adbadeea1fead3030d9d
Joost Diepenmaat <joost@zeekat.nl>
2012-01-13 08:53:44 +0100

    prepare 0.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flutter "0.0.6"
+(defproject flutter "0.0.7"
   :description "consistent, extensible, hiccup-based HTML form fields"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hiccup "0.3.4"]])
